f6a4e79754c9129f027270917d8f12bbafae3073
Maksym <s31138@pjwstk.edu.pl>
2025-07-01 17:05:10 +0200

    fix: add missing sidebar ling to categories

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -51,7 +51,7 @@ const data = {
         },
         {
             title: "Categories",
-            url: "#",
+            url: "/categories",
             icon: IconChartBar,
         },
         {
