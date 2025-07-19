e9bf6c242d99c72b1f62c2d757346baeb6f4e2fb
Kopenkin Dmitrii <dmitrii@moveworkforward.com>
2025-07-19 13:45:11 +0200

    temp: add logging to build step

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
+const apiUrl = process.env.VITE_API_URL || "http://localhost:8000";
+const aiApiUrl = process.env.VITE_AI_API_URL || "http://localhost:8001";
+
+console.log({ apiUrl, aiApiUrl })
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
