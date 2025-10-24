c82185af442fe3129aca315a94a0c12382d441ed
Kopenkin Dmitrii <dmitrii@moveworkforward.com>
2025-10-24 17:41:47 +0200

    Add initialization for barcode detector in polyfills and expose it to the global window object for improved barcode scanning functionality.

--- src/lib/polyfills.ts
+++ src/lib/polyfills.ts
@@ -2,3 +2,7 @@ import { BarcodeDetector } from "barcode-detector/ponyfill";
 (
   globalThis as unknown as { BarcodeDetector: typeof BarcodeDetector }
 ).BarcodeDetector ??= BarcodeDetector;
+
+(
+  window as unknown as { initBarcodeDetector: () => Promise<void> }
+).initBarcodeDetector();
