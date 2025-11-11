85a0e981fb886a59531b8c33c5251f88286e8176
Kopenkin Dmitrii <dmitrii@moveworkforward.com>
2025-11-11 17:03:08 +0100

    refactor: update imports in SiteHeader component for better organization; conditionally render Logo based on POS state

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -2,10 +2,11 @@ import { ChevronLeft } from "lucide-react";
 import { Link, useLocation } from "wouter";
 import { SidebarTrigger } from "~/components/ui/sidebar";
 import { useIsPos } from "~/hooks/use-is-pos";
-import { LanguageToggle } from "./common/language-toggle";
-import { OnlineIndicator } from "./common/online-indicator";
-import { ThemeToggle } from "./common/theme-toggle";
-import { Button } from "./ui/button";
+import { LanguageToggle } from "~/components/common/language-toggle";
+import { OnlineIndicator } from "~/components/common/online-indicator";
+import { ThemeToggle } from "~/components/common/theme-toggle";
+import { Button } from "~/components/ui/button";
+import { Logo } from "~/components/common/logo";
 
 export function SiteHeader() {
   const [isPos] = useIsPos();
@@ -14,18 +15,28 @@ export function SiteHeader() {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        {!isPos && <SidebarTrigger className="-ml-1" />}
+        {!isPos ? (
+          <>
+            <SidebarTrigger className="-ml-1" />
+          </>
+        ) : (
+          <>
+            <Logo className="h-7" />
+          </>
+        )}
         {splits.length > 1 && (
-          <Button
-            asChild
-            variant="ghost"
-            size="icon-sm"
-            className="-ml-1 size-7"
-          >
-            <Link href={`~/${splits.slice(0, -1).join("/")}`}>
-              <ChevronLeft className="size-4" />
-            </Link>
-          </Button>
+          <>
+            <Button
+              asChild
+              variant="ghost"
+              size="icon-sm"
+              className="-ml-1 size-7"
+            >
+              <Link href={`~/${splits.slice(0, -1).join("/")}`}>
+                <ChevronLeft className="size-4" />
+              </Link>
+            </Button>
+          </>
         )}
         <div className="ml-auto flex items-center gap-2">
           <OnlineIndicator className="size-7" />
