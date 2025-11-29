eebf9b103b45da2c79ce86099d11d23da28cb943
Kopenkin Dmitrii <dmitrii@moveworkforward.com>
2025-11-29 17:09:28 +0100

    feat: implement printer control functionality with new usePrinter hook; enhance PrinterStatus component to support test print and cash drawer actions via dropdown menu

--- src/components/common/printer-status.tsx
+++ src/components/common/printer-status.tsx
@@ -1,4 +1,9 @@
-import { IconPrinterOff } from "@tabler/icons-react";
+import {
+  IconCash,
+  IconDotsVertical,
+  IconPrinter,
+  IconPrinterOff,
+} from "@tabler/icons-react";
 import { PrinterCheckIcon } from "lucide-react";
 import { Badge } from "~/components/ui/badge";
 import { usePrinterSettings } from "~/hooks/printer/use-printer-settings";
@@ -7,6 +12,33 @@ import { FieldGroup, FieldLabel } from "../ui/field";
 import { Input } from "../ui/input";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { useEffect, useRef, useState } from "react";
+import { usePrinter } from "~/hooks/printer/use-printer";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Receipt } from "~/lib/receipt";
+
+const EXAMPLE_RECEIPT = `{image:iVBORw0KGgoAAAANSUhEUgAAAQAAAAA8AgMAAAD004yXAAAACVBMVEVwAJsAAAD///+esS7BAAAAAXRSTlMAQObYZgAAAZtJREFUSMftlkGOwyAMRW0J76kE97GlZu9KcP+rzCekKak6bWdGmrZS6KIG/7yAbQhEe+tN6qUpDZ1KPHT8SnhpeaP6FlA2wicBsgOeBpTF6gDfZHgj9Jt1sAMeAYYE/RFQngYMuX49gD8fMObjkwB+++h+DeB3x/qdvfD/AP4QwPXX+ceAUdV6Y7K/3Vr7rWhv73ZN9XVHzOUdlm6v9ay3pM1eLT+Ppv63BWjz8jJU0vpUWgGs5yfihrN451G+lq7i2bkF8Bagw3Qv0hEQKGTPPjnGxJtZrSIcJy5ciJ3JStbarjrgwOtVK7Z1iLFOOgOSR3WstTqMqCcVFlITDnhhNhMKzJNIA3iEBr1uaAfArA7bSWdAkRrIsJSMrwXbJBowEhogwe9F+NilC8D1oIyg43fA+1WwKpoktXUmsiOCjxFMR+gEBfwcaJ7qDBBWNTVSxKsZZhwkUgkppEiJ1VOIQdCFtAmNBQg9QWj9POQWFW4Bh0HVipRIHlpEUVzUAmslpgQpVcS0SklcrBqf03u/UvVhLd8H5Hffil/ia4Io3warBgAAAABJRU5ErkJggg==}
+
+          Ichigaya Terminal
+       1-Y-X Kudan, Chiyoda-ku
+-------------------------------------
+03-18-2024 12:34
+{border:line}
+^RECEIPT
+{border:space}
+{width:*,2,10}
+HAMBURGER              | 2|     24.00
+COFFEE                 | 2|     12.00
+-------------------------------------
+{width:*,20}
+^TOTAL             |           ^36.00
+CASH               |            40.00
+CHANGE             |             4.00
+{code:20240318123456;option:code128,48}`;
 
 export const PrinterStatus = ({
   ...props
@@ -15,6 +47,7 @@ export const PrinterStatus = ({
   const [vendorId, setVendorId] = useState(settings?.vendor_id || 0);
   const [productId, setProductId] = useState(settings?.product_id || 0);
   const hasSetSettings = useRef(false);
+  const { cut, print, cashdraw } = usePrinter();
 
   useEffect(() => {
     if (
@@ -74,7 +107,39 @@ export const PrinterStatus = ({
               onChange={(e) => setProductId(Number(e.target.value))}
             />
           </FieldGroup>
-          <Button type="submit">Save</Button>
+          <div className="flex gap-2 flex-nowrap">
+            <Button type="submit" className="flex-1">
+              Save
+            </Button>
+            {hasSettings && (
+              <DropdownMenu modal={false}>
+                <DropdownMenuTrigger asChild>
+                  <Button type="button" variant="ghost" size="icon">
+                    <IconDotsVertical className="size-4" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem
+                    onClick={async () => {
+                      const example = Receipt.from(
+                        EXAMPLE_RECEIPT,
+                        "-c 42 -p escpos -n"
+                      );
+                      const instructions = await example.toCommand();
+                      print(Receipt.prepareCommand(instructions)).then(() => cut());
+                    }}
+                  >
+                    <IconPrinter className="size-4" />
+                    Test Print
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={cashdraw}>
+                    <IconCash className="size-4" />
+                    Test cashdraw
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            )}
+          </div>
         </form>
       </PopoverContent>
     </Popover>
